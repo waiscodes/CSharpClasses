ba681e57bb0a0bb2c5679bcca33a016f63632753
waiscodes <b.wais@outlook.com>
2020-10-21 09:39:55 -0600

    made a class

---
+++ CSharpOOP/CSharpOOP/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOOP
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+    }
+}
---
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.WriteLine("State your name cuz");
+            person.FirstName = Console.ReadLine().Trim();
+
+            Console.WriteLine("State your name cuz");
+            person.LastName = Console.ReadLine().Trim();
+
+            Console.WriteLine($"hello {person.FirstName} {person.LastName}");
+
+        }
+    }
+}
